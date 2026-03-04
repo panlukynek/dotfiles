ff76d644fcbb79c2ee7cbf29a96e8ffbcdb144da
panlukynek <panlukynek@proton.me>
2026-03-04 19:17:30 +0100

    waybar

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,7 +1,8 @@
 killall waybar
 if [[ $USER = "tydlifon" ]]
 then
-    waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
+	# waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
+	waybar -c ~/.config/waybar/pill.jsonc -s ~/.config/waybar/pill.css
 else
-    waybar &
-fi 
+	waybar &
+fi
