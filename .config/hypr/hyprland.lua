4f9f323662771112b4644df72f78ed93e572e5e4
panlukynek <panlukynek@proton.me>
2026-05-06 02:35:04 +0200

    apofejoasvt

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,6 @@
+local terminal = kitty
+local fileManager = "kitty --hold yazi"
+local fileManager = thunar
+local menu = "wofi --show drun"
+
+hl.bind("SUPER, T", hl.dsp.exec_cmd())
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,34 +72,22 @@ require("gruvbox").setup({
   dim_inactive = false,
   transparent_mode = true,
 })
+
+-- require("oldworld").setup({
+-- 	styles = {
+-- 		booleans = { italic = true, bold = true },
+-- 	},
+-- 	integrations = {
+-- 		telescope = true,
+-- 		indent_blankline = true,
+-- 		lazy = true,
+-- 		lsp = true,
+-- 		markdown = true,
+-- 		neo_tree = true,
+-- 		treesitter = true
+-- 	}
+-- })
+
 vim.cmd("colorscheme gruvbox")
 
 vim.o.background = "dark"
-
--- require('kanagawa').setup({
---     compile = false,             -- enable compiling the colorscheme
---     undercurl = true,            -- enable undercurls
---     commentStyle = { italic = true },
---     functionStyle = {},
---     keywordStyle = { italic = true},
---     statementStyle = { bold = true },
---     typeStyle = {},
---     transparent = false,         -- do not set background color
---     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
---     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
---     colors = {                   -- add/modify theme and palette colors
---         palette = {},
---         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
---     },
---     overrides = function(colors) -- add/modify highlights
---         return {}
---     end,
---     theme = "dragon",            -- Load "wave" theme
---     background = {               -- map the value of 'background' option to a theme
---         dark = "dragon",         -- try "dragon" !
---         light = "lotus"
---     },
--- })
---
--- -- setup must be called before loading
--- vim.cmd("colorscheme kanagawa")
